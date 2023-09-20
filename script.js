bc27fbdd400de3976bcf2a8b3d79a332b355302b
Sevanthcherry <122200476+Sevanthcherry@users.noreply.github.com>
2023-09-20 11:56:34 +0530

    Header section completed

---
+++ script.js
@@ -0,0 +1,91 @@
+let movies = [
+
+    {
+        name:"Wrong Turn 6",
+        poster:"https://upload.wikimedia.org/wikipedia/en/thumb/3/32/WrongTurn6LastResortPoster.jpg/220px-WrongTurn6LastResortPoster.jpg",
+        rating: 7.8
+    },
+    
+    {
+        name:"Baahubali 1",
+        poster: "https://pics.filmaffinity.com/Baahubali_The_Beginning-989063887-large.jpg",
+        rating: 9.5
+    },
+
+    {
+        name:"Baahubali 2",
+        poster: "https://rukminim1.flixcart.com/image/850/1000/kzygpzk0/poster/r/q/s/large-movie-bahubali-2-posters-on-large-print-36x24-inches-original-imagbukjsjtksjsg.jpeg?q=20",
+        rating: 9.7
+    },
+
+    {
+        name:"Avengers Infinity War",
+        poster: "https://rukminim2.flixcart.com/image/850/1000/ky90scw0/sticker/a/a/x/medium-avengers-infinity-war-decorative-for-poster-18-ps-32-original-imagagzvhmsudcfg.jpeg?q=90",
+        rating: 8.8
+    },
+
+    {
+        name:"Raghuvaran B.Tech",
+        poster: "https://m.media-amazon.com/images/S/pv-target-images/007c2a404b512d15e771c79044b49d7c01880fd2f7372fd30653607dc4878875.jpg",
+        rating: 8.8
+    },
+
+    {
+        name:"KGF Chapter 2",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2sdZLeZoFKzBmkcNxvaNB7Podj7sVA_Pz0g&usqp=CAU",
+        rating: 9.0
+    },
+
+    {
+        name:"Krrish",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzRlNDJmOWUtMTM5Ni00ZjNlLWEwZjYtNTY1ZGEzNjE3OWMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg",
+        rating: 8.8
+    },
+
+    {
+        name:"Dhoom 2",
+        poster: "https://upload.wikimedia.org/wikipedia/en/1/13/Dhoom_2_%282006_film%29_poster.jpg",
+        rating: 7.5
+    },
+
+    {
+        name:"Eega",
+        poster: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201305/eega-poster_660_051513034523.jpg?VersionId=XH0eDfHn2tgpVaDfMRwdIYkNQ0RDHNPT",
+        rating: 8.8
+    },
+
+    {
+        name:"A Aa",
+        poster: "https://m.media-amazon.com/images/M/MV5BYzE0OTgyZjgtYzUxNC00NDRiLTlhZTQtZTQyOTUwNTQwNjg4XkEyXkFqcGdeQXVyNTE0NDY5Njc@._V1_.jpg",
+        rating: 8.0
+    },
+
+    {
+        name: "24",
+        poster: "https://www.cinema.com.my/images/movies/2016/72400_450.jpg",
+        rating: 7.8
+    },
+
+    {
+        name: "Oppenheimer",
+        poster: "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg",
+        rating: 8.2
+    },
+
+    {
+        name: "Vikram",
+        poster: "https://feeds.abplive.com/onecms/images/uploaded-images/2021/07/10/273e787ba809e725948334340c658e68_original.jpg",
+        rating: 8.8
+    }
+
+
+];
+
+
+
+
+function searchMovie()
+{
+    let movieName = document.getElementById('search').value;
+    console.log(movieName);
+}
